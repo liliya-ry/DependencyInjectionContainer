e1ac58d1988414bb50d0d143cb85177bf640a6ea
liliya_ry <lilichka.yordanova@gmail.com>
2023-01-12 19:08:41 +0200

    update

--- src/test/java/org/example/TestClasses.java
+++ src/test/java/org/example/TestClasses.java
@@ -23,10 +23,12 @@ public class TestClasses {
 
     static class E {
         A aField;
+        float percent;
 
         @Inject
-        public E(A afield) {
+        public E(A afield, @Named("percent") float percent) {
             this.aField = afield;
+            this.percent = percent;
         }
     }
 
@@ -39,16 +41,45 @@ public class TestClasses {
         @Inject @Named String email;
     }
 
-    interface Initializer {
-        void init() throws Exception;
-    }
-
-    class FSI implements Initializer {
+    static class FSI implements Initializer {
         @Inject @Named String email;
 
         @Override
         public void init() throws Exception {
             email = "mailto:" + email;
+        }
+    }
+
+    static class G {
+        A aField;
+        B bField;
+
+        @Inject
+        public G(A afield) {
+            this.aField = afield;
+        }
+
+        @Inject
+        public G(B bfield) {
+            this.bField = bfield;
+        }
+    }
+
+    static class H {
+        boolean flag;
+
+        @Inject
+        public H(boolean flag) {
+            this.flag = flag;
+        }
+    }
+
+    static class I {
+        boolean flag;
+
+        @Inject
+        public I(@Named("flag") boolean flag) {
+            this.flag = flag;
         }
     }
 }
