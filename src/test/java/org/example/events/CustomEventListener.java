bfe8800a470ca632e0c282e43f792cfa4047ea48
liliya_ry <lilichka.yordanova@gmail.com>
2023-01-23 08:59:01 +0200

    update

--- src/test/java/org/example/events/CustomEventListener.java
+++ src/test/java/org/example/events/CustomEventListener.java
@@ -1,7 +1,10 @@
 package org.example.events;
 
+import org.example.annotations.Async;
+
 public class CustomEventListener {
     @EventListener
+    @Async
     public void handleStringEvent(CustomEvent event) {
         System.out.println("Received custom String event - " + event.getMessage());
     }
